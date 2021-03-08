9ecb57161630ae2a5d025e969fecb68edd63b851
Teamatlantis21 <80259767+Teamatlantis21@users.noreply.github.com>
2021-03-08 17:24:07 +0300

    генератор карт

--- generator.pl
+++ generator.pl
@@ -11,10 +11,10 @@ print "$y.ox\n";
 for (my $i = 0; $i < $y; $i++) {
 	for (my $j = 0; $j < $x; $j++) {
 		if (int(rand($y) * 2) < $density) {
-			print " o ";
+			print "o";
 		}
 		else {
-			print " . ";
+			print ".";
 		}
 	}
 	print "\n";
